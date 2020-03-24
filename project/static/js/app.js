d7a4aa08c12fe3155312d1f7e859e10225e218e1
miki tomioka <mikitomioka@miki.local>
2020-03-24 21:59:31 +0900

    jsを修正

--- project/static/js/app.js
+++ project/static/js/app.js
@@ -1,40 +1,48 @@
 window.onload = function(){
-    $('.checkbox').change(function() {
-        let id = $(this).attr('name');
-        if ($(this).prop('checked')) {
-            window.location.href = "/complete/" + id;
-        }else {
-            window.location.href = "/uncomplete/" + id;
-        };
-    })
+    $('.checkbox').change(change_complete)
+    $('.edit_icon').click(edit_mode)
+    $('.form_btn').click(edit_btn)
+    $('.form_area').change(form_btn_disabled)
+}
 
-    $('.edit_icon').click(function() {
-        if ($('.form_btn').text() === '追加') {
-            console.log('編集モード')
-            let data = $(this).data();
-            $('.form_area').attr({
-                placeholder: data.item,
-                'data-id': data.id,
-            });
-            $('.form_btn').attr('type', 'button').text('編集');
-        }else {
-            console.log('解除')
-            $('.form_area').removeAttr('placeholder value')
-            $('.form_btn').attr('type', 'submit').text('追加');
-        }
-        return false
-    })
+function change_complete() {
+    let id = $(this).attr('name');
+    if ($(this).prop('checked')) {
+        window.location.href = "/complete/" + id;
+    }else {
+        window.location.href = "/uncomplete/" + id;
+    };
+}
 
-    $('.form_btn').click(function() {
-        let id = $('.form_area').data('id');
-        window.location.href = "/edit/" + id + '/' + $('.form_area').val();
-    })
 
-    $('.form_area').change(function() {
-        if ($('.form_area').val() != '') {
-            $('.form_btn').prop('disabled', false);
-        }else {
-            $('.form_btn').prop('disabled', true);
-        }
-    })
+function edit_mode() {
+    if ($('.form_btn').text() === '追加') {
+        console.log('編集モード')
+        let data = $(this).data();
+        $('.form_area').attr({
+            placeholder: data.item,
+            'data-id': data.id,
+        });
+        $('.form_btn').attr('type', 'button').text('編集');
+    }else {
+        console.log('解除')
+        $('.form_area').removeAttr('placeholder value')
+        $('.form_btn').attr('type', 'submit').text('追加');
+    }
+    return false
+}
+
+
+function edit_btn() {
+    let id = $('.form_area').data('id');
+    window.location.href = "/edit/" + id + '/' + $('.form_area').val();
+}
+
+
+function form_btn_disabled() {
+    if ($('.form_area').val() != '') {
+        $('.form_btn').prop('disabled', false);
+    }else {
+        $('.form_btn').prop('disabled', true);
+    }
 }
